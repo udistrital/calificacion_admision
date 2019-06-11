719d6bc12121ba646f84c997e62a63e32774a83d
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-06-11 15:54:52 -0500

    Se agregan variables de entorno para generalizar el API

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	orm.Debug = true
-	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
@@ -35,6 +35,7 @@ func main() {
 	logPath += beego.AppConfig.String("logPath")
 	logPath += "\"}"
 	logs.SetLogger(logs.AdapterFile, logPath)
+
 	beego.ErrorController(&customerror.CustomErrorController{})
 
 	apistatus.Init()
