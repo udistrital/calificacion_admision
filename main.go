0cf3bcf32abaa30f818e710cf766930cfbaeb197
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-06-17 10:21:17 -0500

    Se cambia main.go de acuertdo a los lineamento del repo titan_api_crud

--- main.go
+++ main.go
@@ -12,13 +12,16 @@ import (
 	"github.com/udistrital/utils_oas/customerror"
 )
 
-func main() {
-	orm.Debug = true
-	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
+func test() {
 	if beego.BConfig.RunMode == "dev" {
+		orm.Debug = true
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+}
+
+func init() {
+	test()
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
@@ -31,13 +34,17 @@ func main() {
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
+}
+
+func main() {
+	orm.Debug = true
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 	logPath := "{\"filename\":\""
 	logPath += beego.AppConfig.String("logPath")
 	logPath += "\"}"
 	logs.SetLogger(logs.AdapterFile, logPath)
-
+	apistatus.Init()
 	beego.ErrorController(&customerror.CustomErrorController{})
 
-	apistatus.Init()
 	beego.Run()
 }
